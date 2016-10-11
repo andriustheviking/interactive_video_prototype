db11e1352f998964afdadace0906da07f359a9f6
Andrius <andriusvk@gmail.com>
2016-10-11 17:22:40 -0400

    merge interactive video and container

--- js/script.js
+++ js/script.js
@@ -1,71 +1,139 @@
 //Prototype 2 JavaScript
 
+//GLOBALS
+var autoPlay = true;
+
 $(document).ready(function(){
+	
+
 	//element vars
 	var nameInput = $('#name-input'),
 		imageInput = $('#image-input'),
-		preview = $('#preview-container');
-	
+		preview = $('#preview-container'),
+		createBtn = $('#btn-create'),
+		imageName = $('#img-name'),
+		inputContainer = $('#initial-user-input'),
+		videoContainer = $('#i-video-container');
+
+	//interactive video elements
+	var video = $('#video')[0],
+		overlay = $('#overlay'),
+		playButton = $('#btn-play'),
+		textField = $('#textfield'),
+		clearButton = $('#btn-clear'),
+		overlayText = $('#overlaytext');
+
+
+	//vars to store user submitted info
+	var userImage;
+	var userName;
+
+	//if auto play, set playPause btn to Pause, else playe
+	autoPlay ? playButton.html("Pause") : playButton.html("Play")
+
+	//define dropzone and handlers
 	var	dragDrop = $('#dragzone')[0];
 		dragDrop.addEventListener('dragenter', dragenter, false);
 		dragDrop.addEventListener('dragover', dragover, false);
 		dragDrop.addEventListener('drop', drop, false);
-
-	//handles when users drag object into dropzone
-	function dragenter(e) {
-		  e.stopPropagation();
-		  e.preventDefault();
-		}
-
-	//handles when users drag object over dropzone
-	function dragover(e) {
-	  e.stopPropagation();
-	  e.preventDefault();
-	}
-
-	//handles when users drop file into dropzone
-	function drop(e) {
-	  e.stopPropagation();
-	  e.preventDefault();
-
-	  var dt = e.dataTransfer;
-	  var files = dt.files		
-
-	  handleFiles(files[0]);
-	}
 
 	//if user manually uploads input
 	imageInput.change(function(){
 		handleFiles(this.files[0]);
 	});
 
-	//create image preview
+	//when button is pressed, fire playPause
+	playButton.click( function(){
+		playPause()
+	});
+	//function to be called when Play/Pause button is pressed
+	function playPause(){
+	    video.paused ? (
+	        video.play(),
+	        playButton.html("Pause")
+	    ) : (
+	        video.pause(),
+	        playButton.html("Play")
+	    );};
+
+	//when user hits 'Create Video', hide input, show video and play
+	createBtn.click( function(){
+		//get username
+		userName = nameInput.val();
+		
+		inputContainer.hide();
+		videoContainer.show();		
+		
+		playVideo();
+	});	
+
+	function playVideo(){
+		if(autoPlay)
+			video.play();
+		
+	};
+
+
+	//handles when users drag object into dropzone
+	function dragenter(e) {
+		e.stopPropagation();
+		e.preventDefault();
+	}
+
+	//handles when users drag object over dropzone
+	function dragover(e) {
+	    e.stopPropagation();
+	    e.preventDefault();
+	}
+
+	//handles when users drop file into dropzone
+	function drop(e) {
+	    e.stopPropagation();
+	    e.preventDefault();
+
+	    var dt = e.dataTransfer;
+	    var files = dt.files		
+
+		handleFiles(files[0]);
+	}
+
+	//create image preview, returns image variable
 	function handleFiles(file){
-		console.log(file);
-		var imageType = /^image\//;
 		//make sure file is an image
+		var imageType = /^image\//;
 		if (!imageType.test(file.type)){
 			alert('Uploade image');
 			return false;
 		}
 
 		var img = document.createElement('img');
+
+		//add css class to image
 		img.classList.add('image-preview');
-		img.file = file;
-		
-		preview.append(img);
+
+		//insert img element to preview div
+		preview.html(img);
+
+		imageName.html(file.name);
 
 		var reader = new FileReader();
+		
+		//tells reader to read file (firing onload)
+		reader.readAsDataURL(file);
+
 		//tells the reader what to do when onload is fired
 		reader.onload = (function(aImg) { 
 			return function (e) { 
 				aImg.src = e.target.result; 
 			};
 		})(img);
-
-		//tells reader to read file (firing onload)
-		reader.readAsDataURL(file);
-						
-		return true;	
+		
+		//stores userImage as $object		
+		userImage = $('.image-preview');	
+		
+		//adds alt tag
+		userImage.attr('alt',file.name);
+		
+		return true;
 	}
 });
